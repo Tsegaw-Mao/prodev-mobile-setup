59cda4e6e21ff99fc4aaa8b585cf107b38c91523
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-08-31 11:58:01 +0300

    task 2

---
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Entry Screen - Awesome</Text>
+      <View>
+        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+        <Text style={styles.smallText}>ALX is awesome</Text>
+      </View>
+    </View>
+
+  );
+}
---
+++ prodev-mobile-app-0x01/app/styles.ts
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#90caf9",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  largeText: {
+    fontSize: 30,
+    color: "#f44336",
+    marginBottom: 5,
+    fontWeight: "700",
+    fontVariant: ["small-caps"],
+  },
+  mediumText: {
+    fontSize: 20,
+    color: "#9c27b0",
+    marginBottom: 10,
+    fontWeight: "500",
+    textAlign: "right",
+  },
+  smallText: {
+    fontSize: 15,
+    color: "#2196f3",
+    fontWeight: "400",
+    textAlign: "center",
+  },
+});
